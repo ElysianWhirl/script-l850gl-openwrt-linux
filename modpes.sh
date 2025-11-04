76ead07af9b3ed3767d37cc48681dfbf8e06bea4
ElysianWhirl <158003859+ElysianWhirl@users.noreply.github.com>
2025-11-04 20:33:58 +0700

    Update modpes.sh

--- modpes.sh
+++ modpes.sh
@@ -5,5 +5,6 @@
 #===COPYRIGHT SCRIPT=======#
 
 interface=wwan0
+portAT=ACM0
 
-echo -e "AT+CFUN=4\r" > "/dev/ttyACM2" && sleep 1 && ifdown $interface && sleep 1 && echo -e "AT+CFUN=1\r" > "/dev/ttyACM2" && sleep 1 && ifup $interface 
+echo -e "AT+CFUN=4\r" > "/dev/tty$portAT" && sleep 1 && ifdown $interface && sleep 1 && echo -e "AT+CFUN=1\r" > "/dev/tty$portAT" && sleep 1 && ifup $interface 
